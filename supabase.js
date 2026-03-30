aee06193095af8d55c17fe0ff2745153a956dece
mia-technician <miamenglu@gmail.com>
2026-03-29 18:55:39 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@
 
 const supabase = supabase.createClient(
   "https://dmrskyskryrdclknezth.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtcnNreXNrcnlyZGNsa25lenRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNzE2MzgsIsInN1YiI6IjE3NzQzNzE2MzgiLCJleHAiOjI0ODA5NTE2Mzh9.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtcnNreXNrcnlyZGNsa25lenRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNzE2MzgsImV4cCI6MjA4OTk0NzYzOH0.Dnced4YAY24fE6kLxOe9YuR6Wrgt8pYcqKcjMFWDrY8"
 );
 
 window.supabase = supabase;
