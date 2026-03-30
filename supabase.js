152a7dfb46f9e3ac717260de53a0b4e3dbe354ed
mia-technician <miamenglu@gmail.com>
2026-03-29 18:38:02 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
-// 正确 ✅ 全局共享登录
+// ✅ 正确、干净、可共享
 window.supabase = supabase.createClient(
   "https://dmrskyskryrdclknezth.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtcnNreXNrcnlyZGNsa25lenRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNzE2MzgsImV4cCI6MjA4OTk0NzYzOH0.Dnced4YAY24fE6kLxOe9YuRc8pYpqKcqKcjMFWDrY8"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtcnNreXNrcnlyZGNsa25lenRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNzE2MzgsImV4cCI6MjA4OTk0NzYzOH0.Dnced4YAY24fE6kLxOe9YuR6Wrgt8pYcqKcjMFWDrY8"
 );
